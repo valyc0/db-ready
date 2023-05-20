e4f48095db57fcf608909624a87ed35153b3754d
valyc0 <valyc0@gmail.com>
2023-05-20 10:15:49 +0000

    first

---
+++ mysql8/install.sh
@@ -0,0 +1,11 @@
+docker run -d \
+  --name some-mysql \
+  -e MYSQL_ROOT_PASSWORD=toor \
+  -e MYSQL_DATABASE=mydb \
+  -v $PWD/database.sql:/docker-entrypoint-initdb.d/database.sql \
+  -p 3306:3306 \
+  --cap-add SYS_NICE \
+  mysql:8 --default-authentication-plugin=mysql_native_password
+
+
+docker run --name myadmin -d --link some-mysql:db -p 8085:80 phpmyadmin/phpmyadmin
