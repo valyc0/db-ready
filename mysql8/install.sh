50a4244f74d23f217a2ae23d7492d2ca1efa7262
valyc0 <91672412+valyc0@users.noreply.github.com>
2025-03-24 18:29:37 +0100

    Update install.sh

--- mysql8/install.sh
+++ mysql8/install.sh
@@ -5,7 +5,7 @@ docker run -d \
   -v $PWD/database.sql:/docker-entrypoint-initdb.d/database.sql \
   -p 3306:3306 \
   --cap-add SYS_NICE \
-  mysql:8 --default-authentication-plugin=mysql_native_password
+  mysql:8.0 --default-authentication-plugin=mysql_native_password
 
 
 docker run --name myadmin -d --link some-mysql:db -p 8085:80 phpmyadmin/phpmyadmin
