c913c651be691ed3039d64e1c3f37a27e39a914d
vawqe <valyc202301@gmail.com>
2025-08-16 14:46:53 +0000

    mod

--- oracle21/start_adminer.sh
+++ oracle21/start_adminer.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -d --rm --init -p 8080:80 --name adminer-oracle --entrypoint /usr/bin/php dockette/adminer:oracle-12 -d memory_limit=256M -d upload_max_filesize=2048M -d post_max_size=2048M -S 0.0.0.0:80
+docker run -d --rm --init -p 8080:80 --name adminer-oracle --entrypoint /usr/bin/php dockette/adminer:oracle-12 -d memory_limit=256M -d upload_max_filesize=2048M -d post_max_size=2048M -d extension=oci8 -S 0.0.0.0:80
